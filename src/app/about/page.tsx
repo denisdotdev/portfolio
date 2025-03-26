349f31ca4f9b3de91f968320a97caaa2928aa9eb
Denis Lessard <DenisLessard91@gmail.com>
2025-03-25 21:51:28 -0400

    update img

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,24 @@
 import Experience from '@/components/sections/experience'
 import Skills from '@/components/sections/skills'
 
+import Image from 'next/image'
+
 export default function About() {
   return (
     <div className="font-base">
       <h1 className="mb-8 text-2xl font-heading sm:text-4xl">À propos</h1>
 
       <div className="mr-auto ml-auto mb-8 justify-center">
-        <img
-          className="h-[400px]"
-          src="/portrait.jpeg"
+        <Image
+          src="/portrait.jpg"
           alt="Denis Lessard"
+          style={{
+            width: '50%',
+            height: 'auto',
+          }}
+          width={4000}
+          height={6000}
+          priority={true}
         />
         <span className="text-sm text-center">Photo par <a href="https://www.instagram.com/zoeyy_photographie/">Zoeyy Photographie</a></span>
       </div>
