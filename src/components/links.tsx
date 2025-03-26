5e2b8803cf7ef15a374b1c867fb8dad75fdb5d57
Denis Lessard <DenisLessard91@gmail.com>
2025-03-25 21:14:01 -0400

    add aria label

--- src/components/links.tsx
+++ src/components/links.tsx
@@ -7,22 +7,26 @@ import {
 } from '@icons-pack/react-simple-icons'
 
 export default function Links() {
-  const links: { icon: IconType; href: string }[] = [
+  const links: { icon: IconType; href: string, name: string }[] = [
     {
       icon: SiGmail,
       href: 'mailto:denislessard91@gmail.com',
+      name: 'Gmail'
     },
     {
       icon: SiGithub,
       href: 'https://github.com/denisdotdev',
+      name: 'Github'
     },
     {
       icon: SiLinkedin,
       href: 'https://www.linkedin.com/in/denislessard/',
+      name: 'LinkedIn'
     },
     {
       icon: SiMedium,
       href: 'https://medium.com/@DenisL',
+      name: 'Medium'
     },
   ]
 
@@ -30,7 +34,7 @@ export default function Links() {
     <div className="mr-auto mt-20 flex w-full flex-wrap items-center gap-10">
       {links.map((link, id) => {
         return (
-          <a target="_blank" key={id} href={link.href}>
+          <a target="_blank" key={id} href={link.href} aria-label={link.name}>
             <link.icon title="" />
           </a>
         )
