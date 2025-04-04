5e68ce5fee493d0b259a6ef9165da86aa27d00d7
Denis Lessard <DenisLessard91@gmail.com>
2025-04-04 19:50:39 -0400

    text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="text-2xl font-heading sm:text-4xl">Denis Lessard</h1>
       <p className="mt-2 text-lg sm:text-xl">Développeur web pigiste</p>
       <div className="mt-8 text-base sm:text-lg">
-        <p>Développeur web passionné et créatif, je transforme vos idées en expériences numériques captivantes. Spécialisé dans la création de sites web sur mesure, je combine expertise technique et sensibilité esthétique pour concevoir des solutions qui répondent précisément à vos besoins. Explorez mon portfolio pour découvrir comment je peux donner vie à vos projets. </p>
+        <p>Développeur web passionné et créatif, je transforme vos idées en expériences numériques captivantes. Spécialisé dans la création de sites web sur mesure, je combine expertise technique et sensibilité esthétique pour concevoir des solutions qui répondent précisément à vos besoins.</p> <p>Explorez mon portfolio pour découvrir comment je peux donner vie à vos projets. </p>
       </div>
       <Links />
     </div>
